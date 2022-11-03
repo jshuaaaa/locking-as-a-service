ecd5ef72fc9528924611d8625b6c3a4533ce2476
jshuaaaa <95507083+jshuaaaa@users.noreply.github.com>
2022-11-03 12:15:27 -0400

    pseudo code for functions of Vester

---
+++ contracts/Vester.sol
@@ -0,0 +1,32 @@
+// Function to create the ve-tokenomics
+// 
+
+
+
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract Vester {
+    // State variables
+    uint256 private nextStreamId = 1;
+
+    struct Stream {
+        uint256 streamId;
+        address user;
+        address tokenAddress;
+    }
+    // Functions
+    function createVest() public returns (uint256) {}
+
+    function withdrawFromVest() public {}
+
+    function 
+
+    // View functions
+
+
+}
